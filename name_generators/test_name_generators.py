fe89c9ee567380a19f4075edab83a8a8e8dfbbc1
cbmcdougall <c_mcdougall97@yahoo.co.uk>
2021-11-22 14:57:39 +0000

    add name generator tests

--- name_generators/test_name_generators.py
+++ name_generators/test_name_generators.py
@@ -1,10 +1,15 @@
-import dragon_name as dragon
-import penguin_name as penguin
+from . import dragon_name as dragon
+from . import penguin_name as penguin
+import pytest
 
 class TestDragonName:
-    def test_get_dragon_name():
-        assert dragon.get_dragon_name("Bob", "August") == "BOUST OBAUG"
+    def test_get_dragon_name(self):
+        assert dragon.get_dragon_name("bob", "august") == "BOUST OBAUG"
 
 class TestPenguinName:
-    def test_get_penguin_name():
-        assert penguin.get_penguin_name("Jim", "March") == "Snowball Winterton"
+    def test_get_penguin_name(self):
+        assert penguin.get_penguin_name("jim", "march") == "Blizzy Winterton"
+    def test_get_penguin_name_error(self, capfd):
+        penguin.get_penguin_name("brian", "mar")
+        out, err = capfd.readouterr()
+        assert out == "\nWhoops! Error in generating penguin name\n\n"
