d7ae7e51d632c1ca47ed3ee23b8c9d437933e1fb
cbmcdougall <c_mcdougall97@yahoo.co.uk>
2021-11-22 15:02:41 +0000

    cleanup

--- name_generators/test_name_generators.py
+++ name_generators/test_name_generators.py
@@ -1,6 +1,5 @@
 from . import dragon_name as dragon
 from . import penguin_name as penguin
-import pytest
 
 class TestDragonName:
     def test_get_dragon_name(self):
@@ -9,7 +8,8 @@ class TestDragonName:
 class TestPenguinName:
     def test_get_penguin_name(self):
         assert penguin.get_penguin_name("jim", "march") == "Blizzy Winterton"
-    def test_get_penguin_name_error(self, capfd):
+
+    def test_get_penguin_name_error(self, capsys):
         penguin.get_penguin_name("brian", "mar")
-        out, err = capfd.readouterr()
+        out, err = capsys.readouterr()
         assert out == "\nWhoops! Error in generating penguin name\n\n"
